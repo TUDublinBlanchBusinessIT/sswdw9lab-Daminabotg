64dd0795c3213f93852e6057c6fd3207e4b76358
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-11-12 10:28:07 +0000

    Updated Part 2

--- testCarPolicy2.php
+++ testCarPolicy2.php
@@ -1,15 +1,13 @@
 <?php
 date_default_timezone_set('Europe/Dublin');
 include("CarPolicy2.php");
+
 $initialPremium = 600;
 $myCarpolicy = new CarPolicy("XM123456", $initialPremium);
-
 $myCarpolicy->setDateOfLastClaim("2015-10-10");
 
-echo "years no claims: " . $myCarpolicy->getTotalYearsNoClaims();
-
-echo "Your initial premium is $initialPremium";
-
-echo "Your discounted premium is " . $myCarpolicy->getDiscountedPremium();
-
+echo "Years no claims: " . $myCarpolicy->getTotalYearsNoClaims() . "<br>";
+echo "Your initial premium is €" . $initialPremium . "<br>";
+echo "Your discounted premium is €" . $myCarpolicy->getDiscountedPremium() . "<br>";
 ?>
+
