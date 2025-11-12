ffd99463deabe121b9623b74360da448b8570ef3
B00161001 Daminabo Tom-George <B00161001@mytudublin.ie>
2025-11-12 09:53:13 +0000

    Completed Part A

---
+++ CarPolicy.php
@@ -0,0 +1,36 @@
+<?php
+class CarPolicy
+{
+    private $policyNumber;
+    private $yearlyPremium;
+    private $dateOfLastClaim;
+
+    
+    public function __construct($pn, $yp)
+    {
+        $this->policyNumber = $pn;
+        $this->yearlyPremium = $yp;
+    }
+
+    
+    public function setDateOfLastClaim($dolc)
+    {
+        $this->dateOfLastClaim = $dolc;
+    }
+
+    
+    public function getTotalYearsNoClaims()
+    {
+        $currentDate = new DateTime();
+        $lastDate = new DateTime($this->dateOfLastClaim);
+        $interval = $currentDate->diff($lastDate);
+        return $interval->format("%y");
+    }
+
+    
+    public function __toString()
+    {
+        return "PN: " . $this->policyNumber;
+    }
+}
+?>
